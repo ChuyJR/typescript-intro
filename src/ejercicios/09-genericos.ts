cede9b90b218d7e93ef21f66977edf7e3862ae73
Jesús Ríos <jesusriosaleman@hotmail.com>
2021-06-03 11:04:59 -0500

    Initial Commit

---
+++ src/ejercicios/09-genericos.ts
@@ -0,0 +1,16 @@
+/*
+    ===== Código de TypeScript =====
+*/
+
+function queTipoSoy<T>(argumento: T) {
+    return argumento;
+}
+
+let soyString = queTipoSoy('Hola Mundo');
+let soyNumero = queTipoSoy(100);
+let soyArreglo = queTipoSoy([10, 2, 4, 6]);
+
+let soyExplicito = queTipoSoy<number>(100);
+
+
+
